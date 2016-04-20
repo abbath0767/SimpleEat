b3def1aa3c493802c2c052d663856ea29a461cf7
Abbath0767 <ng@redmadrobot.com>
2016-04-21 00:34:57 +0300

    add function to add new recipe element.

--- app/src/main/java/com/luxary_team/simpleeat/KitchenFragment.java
+++ app/src/main/java/com/luxary_team/simpleeat/KitchenFragment.java
@@ -5,6 +5,7 @@ import android.app.FragmentManager;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +13,11 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class KitchenFragment extends Fragment{
@@ -21,11 +25,14 @@ public class KitchenFragment extends Fragment{
     private EditText mTitleEditText;
     private Spinner mTypeSpinner;
     private Button mAddRecipeButton;
+    private LinearLayout mLinearLayoutContainerElements;
+    private ImageButton mImageButtonAddElement;
 
     private RecipeLab lab;
     private Recipe.RecipeType mRecipeType;
     private Recipe.RecipeType[] recipeTypesWithoutFavorite;
     private Recipe mRecipe;
+    private ArrayList<RecipeElement> mRecipeElements;
 //    private String[] recipeTypes;
 
     @Override
@@ -92,6 +99,67 @@ public class KitchenFragment extends Fragment{
             }
         });
 
+        mLinearLayoutContainerElements = (LinearLayout) view.findViewById(R.id.list_view_element_container);
+
+        mImageButtonAddElement = (ImageButton) view.findViewById(R.id.kitchen_button_add_element);
+        mImageButtonAddElement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final RecipeElement anotherRecipeElement = new RecipeElement();
+
+                LinearLayout anotherLL = (LinearLayout) LayoutInflater.from(getActivity()).inflate(
+                        R.layout.list_item_recipe_element_new, null);
+                anotherLL.setId(View.generateViewId());
+
+                mLinearLayoutContainerElements.addView(anotherLL, mLinearLayoutContainerElements.getChildCount() - 1);
+
+                EditText mEditTextName = (EditText) anotherLL.findViewById(R.id.recipe_element_name_editText);
+                mEditTextName.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        anotherRecipeElement.setName(s.toString());
+                        updateRecipeElements(anotherRecipeElement);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+                    }
+                });
+
+                EditText mEditTextCount = (EditText) anotherLL.findViewById(R.id.recipe_element_count_editText);
+                mEditTextCount.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        anotherRecipeElement.setCount(s.toString());
+                        updateRecipeElements(anotherRecipeElement);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+                    }
+                });
+            }
+        });
+
+
+
         return view;
+    }
+
+    private void updateRecipeElements(RecipeElement element) {
+        Log.d(MainActivity.TAG, "focused: " + mLinearLayoutContainerElements.getFocusedChild().getId());
+
     }
 }
---
+++ app/src/main/java/com/luxary_team/simpleeat/RecipeElement.java
@@ -0,0 +1,24 @@
+package com.luxary_team.simpleeat;
+
+public class RecipeElement {
+    private String mName;
+    private String mCount;
+
+    public String getCount() {
+        return mCount;
+    }
+
+    public void setCount(String count) {
+        mCount = count;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+
+}
