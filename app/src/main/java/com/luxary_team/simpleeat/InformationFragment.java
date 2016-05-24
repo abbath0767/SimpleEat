aa16f65e38f0482cee985eed2e71f638d62cf196
Abbath0767 <ng@redmadrobot.com>
2016-05-24 19:17:57 +0300

    small refactor with unhardcode. need test vk button

--- app/src/main/java/com/luxary_team/simpleeat/InformationFragment.java
+++ app/src/main/java/com/luxary_team/simpleeat/InformationFragment.java
@@ -3,8 +3,10 @@ package com.luxary_team.simpleeat;
 import android.app.Fragment;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,6 +23,13 @@ public class InformationFragment extends Fragment {
         View view = inflater.inflate(R.layout.information_fragment, container, false);
 
         mInfoButton = (Button) view.findViewById(R.id.inf_button);
+        mInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vkintent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.send_my_vk)));
+                startActivity(vkintent);
+            }
+        });
 
         mContactButton = (Button) view.findViewById(R.id.contact_with_us);
         mContactButton.setOnClickListener(new View.OnClickListener() {
@@ -28,12 +37,20 @@ public class InformationFragment extends Fragment {
             public void onClick(View v) {
                 //todo unhardcode
                 String body = getPhoneInfo();
-                Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                shareIntent.setType("text/rfc822");
-                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Отзыв о приложении");
-                shareIntent.putExtra(Intent.EXTRA_TEXT, body);
-                shareIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"ng@redmadrobot.com"});
-                startActivity(Intent.createChooser(shareIntent, "Отправить с помощью..."));
+                Intent shareIntent = ShareCompat.IntentBuilder.from(getActivity())
+                        .setType("text/rfc822")
+                        .setText(body)
+                        .setSubject(getString(R.string.send_subject))
+                        .setChooserTitle(getString(R.string.send_chooser_title))
+                        .setEmailTo(new String[]{getString(R.string.send_my_email)})
+                        .createChooserIntent();
+
+                //// new Intent(Intent.ACTION_SEND);
+//                shareIntent.setType("text/rfc822");
+//                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Отзыв о приложении");
+//                shareIntent.putExtra(Intent.EXTRA_TEXT, body);
+//                shareIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"ng@redmadrobot.com"});
+                startActivity(shareIntent);
             }
         });
 
@@ -43,7 +60,8 @@ public class InformationFragment extends Fragment {
     private String getPhoneInfo() {
         String version;
         String deviceName = Build.MODEL;
-        String osVersion = Build.VERSION.RELEASE + " (SDK " + Build.VERSION.SDK_INT + ")";
+        String osVersion = String.format("%s (SDK %s)", Build.VERSION.RELEASE, Build.VERSION.SDK_INT);
+//        Build.VERSION.RELEASE + " (SDK " + Build.VERSION.SDK_INT + ")";
         try {
             version = String.valueOf(getActivity().getPackageManager()
                     .getPackageInfo(getActivity().getPackageName(), 0)
@@ -52,8 +70,9 @@ public class InformationFragment extends Fragment {
             version = "null";
             Log.d(MainActivity.TAG, "Error in get version");
         }
-        String result = "\n\nВерсия приложения: " + version + "\nУстройство: " + deviceName +
-                "\nВерсия ОС: Android " + osVersion;
+        String result = String.format(getString(R.string.send_device_info), version, deviceName, osVersion);
+//        "\n\nВерсия приложения: " + version + "\nУстройство: " + deviceName +
+//                "\nВерсия ОС: Android " + osVersion;
         return result;
     }
 }
